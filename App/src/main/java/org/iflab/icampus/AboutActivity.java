132b78fcd501e4b7a9ce98ffcc6d2cc0ae4f0615
XuDing <ahtcfg24@163.com>
2015-08-31 23:44:10 +0800

    add aboutActivity but a issue

--- App/src/main/java/org/iflab/icampus/AboutActivity.java
+++ App/src/main/java/org/iflab/icampus/AboutActivity.java
@@ -1,16 +1,63 @@
 package org.iflab.icampus;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class AboutActivity extends ActionBarActivity {
+    private ArrayList<String> aboutItemList;
+    private HashMap<String, String> aboutModMap;
+    private ListView aboutListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        initAboutListView();
+        setItemListener();
+    }
+
+    private void setItemListener() {
+        aboutListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(), "点击了" + aboutModMap.get(aboutItemList.get(position)), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    /**
+     * 初始化ListView
+     */
+    public void initAboutListView() {
+        aboutItemList = new ArrayList<>();
+        aboutItemList.add("学校简介");
+        aboutItemList.add("历史沿革");
+        aboutItemList.add("院系专业");
+        aboutItemList.add("Credits");
+        aboutItemList.add("ifLab");
+        aboutModMap = new HashMap<>();
+        aboutModMap.put(aboutItemList.get(0), "intro");
+        aboutModMap.put(aboutItemList.get(1), "history");
+        aboutModMap.put(aboutItemList.get(2), "colleges");
+        aboutModMap.put(aboutItemList.get(3), "credits");
+        aboutModMap.put(aboutItemList.get(4), "iflab");
+        aboutListView = (ListView) findViewById(R.id.about_listView);
+        aboutListView.setAdapter(new AboutListViewAdapter(aboutItemList, this));
     }
 
     @Override
@@ -33,5 +80,43 @@ public class AboutActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class AboutListViewAdapter extends BaseAdapter {
+        private ArrayList<String> aboutItemList;
+        private Context context;
+        private TextView aboutItemTextView;
+
+
+        public AboutListViewAdapter(ArrayList<String> aboutItemList, Context context) {
+            this.aboutItemList = aboutItemList;
+            this.context = context;
+        }
+
+        @Override
+        public int getCount() {
+            return aboutItemList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+                /*绑定控件资源*/
+            convertView = LayoutInflater.from(context).inflate(R.layout.about_item, null);
+            aboutItemTextView = (TextView) findViewById(R.id.about_item_textView);
+            /*填充列表文字*/
+            aboutItemTextView.setText(aboutItemList.get(position));
+            return convertView;
+        }
     }
 }
