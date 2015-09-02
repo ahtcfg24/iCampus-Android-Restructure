bcb8c206e2b142c1ac2f7c431711021889419aa3
XuDing <ahtcfg24@163.com>
2015-09-02 13:35:50 +0800

    complete the module of about

--- App/src/main/java/org/iflab/icampus/AboutActivity.java
+++ App/src/main/java/org/iflab/icampus/AboutActivity.java
@@ -15,14 +15,26 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.iflab.icampus.http.AsyncHttpIc;
+import org.iflab.icampus.http.UrlStatic;
+import org.iflab.icampus.utils.ACache;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
 public class AboutActivity extends ActionBarActivity {
-    private ArrayList<String> aboutItemList;
-    private HashMap<String, String> aboutModMap;
+    private ArrayList<String> aboutItemList;//存储关于列表各选项的名字
+    private HashMap<String, String> aboutModMap;//存储每个mod的名字
     private ListView aboutListView;
     private Intent intent;
+    private String modName;//mod的名字
+    private String url;//mod对应的网络URL
+    private String aboutDetailsData;//每个mod的详细内容
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +42,16 @@ public class AboutActivity extends ActionBarActivity {
         setContentView(R.layout.activity_about);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         intent = new Intent();
-        initAboutListView();
-        setItemListener();
+        initAboutItemList();
+        aboutListView = (ListView) findViewById(R.id.about_listView);
+        aboutListView.setAdapter(new AboutListViewAdapter(aboutItemList, this));
+        aboutListView.setOnItemClickListener(new ItemListener());
     }
 
     /**
-     * 初始化ListView
+     * 初始化List
      */
-    public void initAboutListView() {
+    private void initAboutItemList() {
         aboutItemList = new ArrayList<>();
         aboutItemList.add("学校简介");
         aboutItemList.add("历史沿革");
@@ -50,23 +64,39 @@ public class AboutActivity extends ActionBarActivity {
         aboutModMap.put(aboutItemList.get(2), "colleges");
         aboutModMap.put(aboutItemList.get(3), "credits");
         aboutModMap.put(aboutItemList.get(4), "iflab");
-        aboutListView = (ListView) findViewById(R.id.about_listView);
-        aboutListView.setAdapter(new AboutListViewAdapter(aboutItemList, this));
+
     }
 
-    private void setItemListener() {
-        aboutListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getApplicationContext(), "点击了" + aboutModMap.get(aboutItemList.get(position)), Toast.LENGTH_SHORT).show();
-                intent.putExtra("mod_name", aboutModMap.get(aboutItemList.get(position)));//把所点击的模块的名称传入
-                intent.putExtra("title", aboutItemList.get(position));//把所点击的模块的标题传入
-                intent.setClass(AboutActivity.this, AboutDetailsActivity.class);
-                startActivity(intent);
-            }
-        });
-    }
+    /**
+     * 通过传入的URL获取网络上的每个mod的详情
+     *
+     * @param url 对应模块的URL
+     */
+    private void getAboutDetailsByUrl(String url) {
+        AsyncHttpIc.get(url, null,
+                        new AsyncHttpResponseHandler() {
+                            @Override
+                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                String introData = new String(responseBody);
+                                try {
+                                    /*因为传入的introData并不符合json格式，因此要先做处理*/
+                                    JSONObject jsonObject = new JSONObject(introData.substring(1, introData.length() - 1));
+                                    aboutDetailsData = jsonObject.getString("introCont");
+                                            /*获取到之后存入缓存里*/
+                                    ACache aCache = ACache.get(getApplicationContext());
+                                    aCache.put(modName, aboutDetailsData);
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
 
+                            }
+
+                            @Override
+                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                                Toast.makeText(getApplicationContext(), "获取数据异常，请重试0.0", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -88,6 +118,24 @@ public class AboutActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class ItemListener implements AdapterView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            modName = aboutModMap.get(aboutItemList.get(position));
+            ACache aCache = ACache.get(getApplicationContext());
+            aboutDetailsData = aCache.getAsString(modName);
+            /*如果缓存里没有这个mod的内容，就从网络获取*/
+            if (aboutDetailsData == null) {
+                url = UrlStatic.ICAMPUSAPI + "/intro.php?mod=" + modName;//构造获取mod详细内容的url
+                getAboutDetailsByUrl(url);
+            }
+            intent.putExtra("aboutDetailsData", aboutDetailsData);//把所点击的模块的数据传入
+            intent.putExtra("title", aboutItemList.get(position));//把所点击的模块的标题传入
+            intent.setClass(AboutActivity.this, AboutDetailsActivity.class);
+            startActivity(intent);
+        }
     }
 
     private class AboutListViewAdapter extends BaseAdapter {
--- App/src/main/java/org/iflab/icampus/AboutDetailsActivity.java
+++ App/src/main/java/org/iflab/icampus/AboutDetailsActivity.java
@@ -3,24 +3,14 @@ package org.iflab.icampus;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.webkit.WebView;
 
-import com.loopj.android.http.AsyncHttpResponseHandler;
-
-import org.apache.http.Header;
-import org.iflab.icampus.http.AsyncHttpIc;
-import org.iflab.icampus.http.UrlStatic;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 public class AboutDetailsActivity extends ActionBarActivity {
     private WebView webView;
-    private String modName;
     private Intent intent;
-    private String url;
+    private String aboutDetailsData;//每个mod的详情
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,33 +20,9 @@ public class AboutDetailsActivity extends ActionBarActivity {
         webView = (WebView) findViewById(R.id.webView);
         intent = getIntent();
         getSupportActionBar().setTitle(intent.getStringExtra("title"));//获取传过来的标题并设为activity的标题
-        modName = intent.getStringExtra("mod_name");//获取传过来的模块名称
-        url = UrlStatic.ICAMPUSAPI + "/intro.php?mod=" + modName;//构造url
-        AsyncHttpIc.get(url, null,
-                        new AsyncHttpResponseHandler() {
-                            @Override
-                            public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-
-                                String introData = new String(responseBody);
-                                try {
-                                    /*因为传入的introData并不符合json格式，因此要先做处理*/
-                                    JSONObject jsonObject = new JSONObject(introData.substring(1, introData.length() - 1));
-                                    String htmlData = jsonObject.getString("introCont");
-                                    webView.loadDataWithBaseURL(null, htmlData, "text/html", "utf-8", null);
-                                } catch (JSONException e) {
-                                    e.printStackTrace();
-                                }
-
-                            }
-
-                            @Override
-                            public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
-                                Log.i("--", "----->失败");
-
-                            }
-                        });
-
+        aboutDetailsData = intent.getStringExtra("aboutDetailsData");//获取传过来的模块数据
+        /*使用loadData会乱码，原因未知*/
+        webView.loadDataWithBaseURL(null, aboutDetailsData, "text/html", "utf-8", null);
 
     }
 
