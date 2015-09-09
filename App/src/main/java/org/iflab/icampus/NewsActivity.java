7ae67578db5c5cfb66c9ef7faaf773df5788971b
XuDing <ahtcfg24@163.com>
2015-09-09 23:48:03 +0800

    start news

--- App/src/main/java/org/iflab/icampus/NewsActivity.java
+++ App/src/main/java/org/iflab/icampus/NewsActivity.java
@@ -1,25 +1,66 @@
 package org.iflab.icampus;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * 新闻模块
  */
 public class NewsActivity extends ActionBarActivity {
 
+    final static String[] NEWSCHANNEL = {"学校要闻", "人才培养", "教学科研", "文化活动",
+            "媒体关注", "校园人物"};
+    final Map<String, String> map = new HashMap<String, String>();
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
         setContentView(R.layout.activity_news);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);//设置ActionBar左上角返回父级Activity，需要在清单中声明父级
 
+
+        map.put("学校要闻", "/xw/zhxw");
+        map.put("人才培养", "/xw/rcpy");
+        map.put("教学科研", "/xw/jxky");
+        map.put("文化活动", "/xw/whhd");
+        map.put("媒体关注", "/xw/mtgz");
+        map.put("校园人物", "/xw/xyrw");
+        button = (Button) findViewById(R.id.backMain);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+                // TODO Auto-generated method stub
+                Intent intent = new Intent(MainActivity.this, ICampus.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP
+                        | Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        FragmentPagerAdapter adapter = new TabPageIndicatorAdapter(
+                getSupportFragmentManager(), map);
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+        pager.setAdapter(adapter);
+        TabPageIndicator indicator = (TabPageIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(pager);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
