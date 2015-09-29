d34c03aeac18c0f3b01d92453a62e03ad8249a54
DD <ahtcfg24@163.com>
2015-09-29 23:55:07 +0800

    todo 2

--- App/src/main/java/org/iflab/icampus/fragment/NewsListFragment.java
+++ App/src/main/java/org/iflab/icampus/fragment/NewsListFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 import com.loopj.android.http.AsyncHttpResponseHandler;
@@ -14,6 +15,11 @@ import org.apache.http.Header;
 import org.iflab.icampus.R;
 import org.iflab.icampus.http.AsyncHttpIc;
 import org.iflab.icampus.http.UrlStatic;
+import org.iflab.icampus.model.NewsItem;
+import org.iflab.icampus.utils.ACache;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 新闻列表
@@ -26,8 +32,11 @@ public class NewsListFragment extends Fragment {
     private String fragmentName;
     private String newsPath;//对应Fragment的相对路径
     private int currentPage;//（滑动scroll时）当前所在的屏幕范围编号
+    private String newsListData;//新闻列表数据
+    private String newsListURL;
+    private ACache aCache;
 //    private List<String> urlList;
-//    private List<NewsItem> newsList;
+    private List<NewsItem> newsList;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -40,34 +49,55 @@ public class NewsListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         rootView=inflater.inflate(R.layout.fragment_news_list, container, false);
-        initNewsList();
+        init();
+        if (newsListData == null) {
+            /*如果缓存没有就从网络获取*/
+            getNewsListDataByURL(newsListURL);
+        } else {
+            jsonNewsListData(newsListData);
+            newsListView.setAdapter(new NewsListAdapter(newsList, NewsListFragment.this));
+        }
         return rootView;
     }
 
     /**
      * 初始化新闻列表
      */
-    private void initNewsList() {
+    private void init() {
+        newsList = new ArrayList<>();
         newsListView=(ListView)rootView.findViewById(R.id.newsListView);
         Bundle bundle = getArguments();
         fragmentName=bundle.getString("fragmentName");
         newsPath =bundle.getString("newsPath");
         currentPage=1;
-        String newsURL =UrlStatic.NEWSAPI+"/api.php?table=newslist&url="+newsPath+"&index="+currentPage;// TODO:currentPage未定义，未实现一次加载三个路径 2015/9/29
+        newsListURL =UrlStatic.NEWSAPI+"/api.php?table=newslist&url="+newsPath+"&index="+currentPage;// TODO:currentPage未定义，未实现一次加载三个路径 2015/9/29
+        aCache = ACache.get(NewsListFragment.this.getActivity());
+        newsListData = aCache.getAsString("newsListData");
+
+
+//        urlList=new ArrayList<>();
+        
+    }
+
+    private void jsonNewsListData(String newsListData) {
+        // TODO: 2015/9/29 json 
+    }
+
+    private void getNewsListDataByURL(String newsURL) {
         AsyncHttpIc.get(newsURL, null, new AsyncHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                System.out.println("。。。。"+new String(responseBody));
+                newsListData= new String(responseBody);
+                jsonNewsListData(newsListData);
+                newsListView.setAdapter(new NewsListAdapter(newsList, NewsListFragment.this));
+                aCache.put("newsListData", newsListData);
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
+// TODO: 2015/9/29  
             }
         });
-
-//        urlList=new ArrayList<>();
-//        newsList = new ArrayList<>();
     }
 
     @Override
@@ -77,4 +107,28 @@ public class NewsListFragment extends Fragment {
     }
 
 
+    private class NewsListAdapter extends BaseAdapter {
+        public NewsListAdapter(List<NewsItem> newsList, NewsListFragment newsListFragment) {
+        }
+
+        @Override
+        public int getCount() {
+            return 0;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            return null;
+        }
+    }
 }
--- App/src/main/java/org/iflab/icampus/model/NewsItem.java
+++ App/src/main/java/org/iflab/icampus/model/NewsItem.java
@@ -1,8 +1,110 @@
 package org.iflab.icampus.model;
 
+import android.graphics.Bitmap;
+
+import java.io.Serializable;
+
 /**
  * 描述新闻条目的类
  * Created at 2015/9/28 23:05.
  */
-public class NewsItem {
+public class NewsItem  implements Serializable{
+    private static final long serialVersionUID = 1L;
+    private String passageId;
+    private String title;
+    private String preview;
+
+    @Override
+    public String toString() {
+        return "NewsItem{" +
+                "isDownloading=" + isDownloading +
+                ", bitmap=" + bitmap +
+                ", detailUrl='" + detailUrl + '\'' +
+                ", icon='" + icon + '\'' +
+                ", updateTime='" + updateTime + '\'' +
+                ", author='" + author + '\'' +
+                ", preview='" + preview + '\'' +
+                ", title='" + title + '\'' +
+                ", passageId='" + passageId + '\'' +
+                '}';
+    }
+
+    public String getPassageId() {
+        return passageId;
+    }
+
+    public void setPassageId(String passageId) {
+        this.passageId = passageId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPreview() {
+        return preview;
+    }
+
+    public void setPreview(String preview) {
+        this.preview = preview;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(String updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getDetailUrl() {
+        return detailUrl;
+    }
+
+    public void setDetailUrl(String detailUrl) {
+        this.detailUrl = detailUrl;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
+
+    public boolean isDownloading() {
+        return isDownloading;
+    }
+
+    public void setIsDownloading(boolean isDownloading) {
+        this.isDownloading = isDownloading;
+    }
+
+    private String author;
+    private String updateTime;
+    private String icon;
+    private String detailUrl;
+    private Bitmap bitmap = null;
+    private boolean isDownloading = true;
 }
